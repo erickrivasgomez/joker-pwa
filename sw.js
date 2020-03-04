d4036815c233e1628c542dfca00cbcac6cb5ffc2
Erick Rivas Gómez <rivaserick@outlook.com>
2020-03-04 09:51:15 -0600

    Deleted links

--- sw.js
+++ sw.js
@@ -10,8 +10,6 @@ const urlsToCache = [
   'https://code.jquery.com/jquery-3.4.1.slim.min.js',
   'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js',
   'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js',
-  'https://github.com/erickrivasgomez/joker-pwa',
-  'https://github.com/erickrivasgomez',
   './style.css',
   './app.js',
   './index.html',
